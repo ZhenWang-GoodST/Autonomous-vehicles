f703b9d4076b2244894fbaff1a8fd2a6ee399c32
wz <2552846766@qq.com>
2020-11-16 22:56:04 +0800

    1

--- download.sh
+++ download.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
-# for line in `cat /home/wz/github/IROS2020-paper-list-main/link.txt`
-# do
-#     echo $line
-#     echo "begin"
-#     # http=${line:10:53}
-#     # echo $http
-#     echo ""
-# done
-cat /home/wz/github/IROS2020-paper-list-main/link.txt | while read line
+cat /home/tl/github/Autonomous-vehicles/link.txt | while read line
 do
-# echo $line
     http=${line:9:53}
-    # echo $http
     name=${line:66}
     name=${name/ |/}
     name=${name// /_}
-    # echo $name
-    file="${name}.pdf !"
-    file="/home/wz/paper/${file}"
+    file="${name}.pdf"
+    file="/home/tl/paper/${file}"
     wget -O ${file} $http 
 done
